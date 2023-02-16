19b4b077db49bc11fd96103fba3a0a298286531d
N.Wen <nwen@clemson.edu>
2023-02-16 00:04:59 -0500

    Switched to the distributed FEM framework

---
+++ SimHubClient.py
@@ -0,0 +1,359 @@
+from typing import List, Dict
+from typing import Any
+
+import os
+import sys
+import enum
+from pathlib import Path
+from concurrent.futures import Future
+import time
+from datetime import datetime
+import uuid
+import json
+
+import rpyc
+from rpyc.core.async_ import AsyncResult
+import yaml
+from tqdm.auto import tqdm
+import xxhash
+from lru import LRU
+from pymongo import MongoClient
+from gridfs import GridFS
+import datajson
+
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+    
+    
+def hash_file(filename: str | Path) -> str:
+    h = xxhash.xxh128()
+    b = bytearray(128*1024)
+    mv = memoryview(b)
+    with open(filename, 'rb', buffering=0) as f:
+        while n := f.readinto(mv):
+            h.update(mv[:n])
+    return h.hexdigest()
+
+
+def hash_dir(path: str | Path = '.', ignore_hidden_files: bool = True) -> Dict[str, str]:
+    # Get file list
+    path = str(path)
+    files = [top + os.sep + f for top, dirs, files in os.walk(path) for f in files]
+    if ignore_hidden_files:
+        files = [f for f in files 
+                 if not any([part.startswith('.') or part.startswith('_') for part in Path(f).parts])]
+
+    return {file.replace(path, '.', 1): hash_file(file) for file in files}
+
+def unpack_task_data(task: Dict[str, Any]) -> Dict[str, Any]:
+    if 'input' in task:
+        task['input'] = datajson.load_json(task['input'])
+    if 'output' in task:
+        task['output'] = datajson.load_json(task['output'])
+    # TODO implement input_files and output_files
+    
+    return task
+    
+    
+class RemoteTaskFuture:
+    def __init__(self, future_netref, input_hash, client):
+        self._future_netref = future_netref
+        self._input_hash = input_hash
+        self._done = False
+        self._result = None
+        self._client = client
+    
+    def done(self) -> bool:
+        if self._future_netref:
+            if self._future_netref.done():
+                self.result()
+                return True
+            else:
+                return False
+        else:
+            return self._done
+                
+    def result(self) -> Any:
+        if self._future_netref:
+            self._result = self._client._db.tasks.find_one({
+                'experiment': self._client.experiment_name,
+                'input_hash': self._input_hash
+            })
+            if 'input' in self._result:
+                self._result['input'] = datajson.load_json(self._result['input'])
+            if 'output' in self._result:
+                self._result['output'] = datajson.load_json(self._result['output'])
+            if 'output_files' in self._result:
+                # TODO Load files
+                ...
+            self._future_netref = None
+            self._done = True
+        return self._result
+        
+        
+class RemoteTaskFutureCollection(List[RemoteTaskFuture]):
+    def wait(self, pull_interval: float = 5., progress_bar: bool = True):
+        if progress_bar:
+            pbar = tqdm(total=len(self))
+        pending_futures = list(self)
+        while len(pending_futures) > 0:
+            completed = []
+            for f in pending_futures:
+                if f.done():
+                    completed.append(f)
+            for f in completed:
+                pending_futures.remove(f)
+            if progress_bar:
+                pbar.update(len(completed))
+            time.sleep(pull_interval)
+        if progress_bar:
+            pbar.close()
+            
+    def results(self):
+        self.wait(progress_bar=False)
+        return [f.result() for f in self]
+    
+class TaskSubmissionStatus(enum.Enum):
+    SUBMITTED = 0  # A new task submitted to server
+    DUPLICATED = 1   # A task shared the same input hash already queued on server
+    EXISTED = 2  # A task already existed in the database
+
+    
+@rpyc.service
+class ClientService(rpyc.Service):
+    @rpyc.exposed
+    def print(self, *args, **kwargs):
+        print(*args, **kwargs)
+        
+    @rpyc.exposed
+    def debug(self, *args, **kwargs):
+        with open('debug.log', 'a') as fp:
+            print(*args, **kwargs, file=fp)
+
+        
+class SimHubClient:
+    def __init__(self, control_node_ip: str, control_node_port: int = 44444, 
+                 database_ip: str | None = None, database_port: int = 40000, 
+                 cache_size: int = 10000):
+        
+        self.control_node_ip = control_node_ip
+        self.control_node_port = control_node_port
+        self.database_ip = database_ip if database_ip else control_node_ip
+        self.database_port = database_port
+        
+        self._conn = rpyc.connect(self.control_node_ip, self.control_node_port, 
+                                  service=ClientService, 
+                                  config={
+                                      'safe_attrs': dict().__dir__() + iter(()).__dir__(),
+                                      'sync_request_timeout': None,
+                                  })
+
+        self._server = self._conn.root
+        
+        self._db = MongoClient(self.database_ip, self.database_port).simdb
+        self._dbfs = GridFS(self._db)
+        
+        self.session_id = self._server.session_id()
+        
+        self.experiment_name = None
+        
+        self._closed = False
+        
+        self.submitted_tasks = list()
+        self.result_cache = LRU(cache_size)
+        
+        self.json_time = []
+        self.query_time = []
+        self.insert_time = []
+        self.submit_time = []
+        
+    def close(self):
+        self._server.close()
+        self._conn.close()
+        self._db.client.close()
+        self._closed = True
+
+        
+    def set_experiment(self, experiment_file: str):
+        experiment_source_dir = Path(experiment_file).parent
+        # Load experiment def file
+        with open(experiment_file, 'r') as fp:
+            exp = yaml.load(fp, Loader=Loader)
+        
+        self.experiment_name = exp['ExperimentName']
+        
+        self._server.set_experiment(exp['ExperimentName'])
+        
+        script_files = []
+        for file in exp['ScriptFiles']:
+            path = (experiment_source_dir / file).resolve(strict=True)
+            print(path)
+            # TODO set permission for server user to read the file
+            script_files.append(str(path))
+        data_files = []
+        for file in exp['DataFiles']:
+            path = (experiment_source_dir / file).resolve(strict=True)
+            print(path)
+            # TODO set permission for server user to read the file
+            data_files.append(str(path))
+        
+        self._server.initialize_experiment_workspace(exp['WorkspaceDir'], script_files, data_files)
+        
+        self._server.create_parallel_app(exp['Run'], exp.get('TaskResources'))
+        
+        self._server.initialize_experiment_executors(exp['PreferedNodes'], exp['Modules'], exp['CondaEnvironment'])
+        
+    def submit_task(self, input_data: Dict[str, Any]):
+        """
+        Submit a task to remote simulator nodes
+        
+        Parameters
+        ----------
+        input_data : Dict[str, Any]
+            input data to simulation script, must be serializable by datajson
+
+        Returns
+        -------
+        string
+            Input hash of input_data, generated by datajson
+        Dict[str, Any] | None
+            If the task is existed, return the task entry from database
+            Otherwise, return None
+
+        Raises
+        ------
+        TODO
+        """
+        input_json, input_hash = datajson.dump_json(input_data, generate_hash=True)
+        
+        # TODO maybe add input_files for large input data
+        
+        # Check local cache
+        if input_hash in self.result_cache:
+            ...  # TODO implement local cache
+        
+        # Check database
+        task = self._db.tasks.find_one({'experiment': self.experiment_name, 
+                                        'input_hash': input_hash})
+        
+        if task:
+            if task['status'] == 'ongoing':
+                if task['session'] == self.session_id:
+                    # Ignore duplicated task
+                    self.submitted_tasks.append((input_hash, TaskSubmissionStatus.DUPLICATED))
+                    return input_hash, None
+                
+                else:
+                    # Takeover tasks from previous sessions
+                    self._db.tasks.update_one(task, {
+                        '$set': {
+                            'session': self.session_id, 
+                            'metrics.submit_timestamp': datetime.now(),
+                        },
+                        '$unset': {'task.assignment': ''},
+                    })
+                    self.submitted_tasks.append((input_hash, TaskSubmissionStatus.SUBMITTED))
+                    return input_hash, None
+                
+            elif task['status'] == 'done':
+                self.submitted_tasks.append((input_hash, TaskSubmissionStatus.EXISTED))
+                task = unpack_task_data(task)
+                return input_hash, task
+            
+            elif task['status'] == 'failed':
+                if task['task']['remaining_retries'] > 0:
+                    self._db.tasks.update_one(task, {
+                        '$set': {
+                            'status': 'ongoing',
+                            'session': self.session_id, 
+                            'metrics.submit_timestamp': datetime.now(),
+                        },
+                        '$unset': {'task.assignment': ''},
+                        '$inc': {'task.remaining_retries': -1},
+                    })
+                    self.submitted_tasks.append((input_hash, TaskSubmissionStatus.SUBMITTED))
+                    return input_hash, None
+                
+                else:
+                    task = unpack_task_data(task)
+                    return input_hash, task
+                
+                
+                
+        # Submit new task
+        self._db.tasks.insert_one({'experiment': self.experiment_name, 
+                                   'session': self.session_id, 
+                                   'input': input_json,
+                                   'input_hash': input_hash, 
+                                   'status': 'ongoing',
+                                   'metrics': {'submit_timestamp': datetime.now()}})
+        
+        self.submitted_tasks.append((input_hash, TaskSubmissionStatus.SUBMITTED))
+        return input_hash, None
+    
+    def wait(self, interval: float = 10, print_stats: bool = True, progress_bar: bool = True):
+        existed = 0
+        duplicated = 0
+        submitted = 0
+        for task in self.submitted_tasks:
+            if task[1] == TaskSubmissionStatus.EXISTED:
+                existed += 1
+            elif task[1] == TaskSubmissionStatus.DUPLICATED:
+                duplicated += 1
+            elif task[1] == TaskSubmissionStatus.SUBMITTED:
+                submitted += 1
+        if print_stats:
+            print(f'Existed: {existed}({existed / len(self.submitted_tasks) * 100:.2f}%)')
+            print(f'Duplicated: {duplicated}({duplicated / len(self.submitted_tasks) * 100:.2f}%)')
+            print(f'New: {submitted}({submitted / len(self.submitted_tasks) * 100:.2f}%)')
+        if progress_bar:
+            pbar = tqdm(total=len(self.submitted_tasks))
+        results = [None] * len(self.submitted_tasks)
+        # Loop/wait until all result done
+        while sum(result is None for result in results) > 0:
+            updated = 0
+            # Iter over each task
+            for i in range(len(self.submitted_tasks)):
+                if results[i]:
+                    continue
+                time.sleep(min(interval / len(self.submitted_tasks), 0.1))
+                input_hash, status = self.submitted_tasks[i]
+                if input_hash in self.result_cache:
+                    results[i] = self.result_cache[input_hash]
+                else:
+                    task = self._db.tasks.find_one({'experiment': self.experiment_name, 
+                                                    'input_hash': input_hash})
+                    if task['status'] == 'done' or task['status'] == 'failed':
+                        task = unpack_task_data(task)
+                        # TODO implement input_files and output_files
+                        self.result_cache[input_hash] = task
+                        results[i] = task
+                if progress_bar and results[i]:
+                    pbar.update(1)
+                    updated += 1
+            # if progress_bar:
+            #     pbar.update(updated)
+            # time.sleep(interval)
+        if progress_bar:
+            pbar.close()
+            
+        successful = []
+        failed = []
+        for result in results:
+            if result['status'] == 'done':
+                successful.append(result)
+            elif result['status'] == 'failed':
+                failed.append(result)
+        if print_stats:
+            print(f'Successful: {len(successful)}({len(successful) / len(self.submitted_tasks) * 100:.2f}%)')
+            print(f'Failed: {len(failed)}({len(failed) / len(self.submitted_tasks) * 100:.2f}%)')
+        
+        return results, successful, failed
+    
+    def get_result(self, input_hash: str) -> Dict[str, Any]:
+        return self.result_cache[input_hash]
+
+    def clear_tasks(self):
+        self.submitted_tasks.clear()
---
+++ elmer_task/elmer_script.py
@@ -0,0 +1,278 @@
+def run(data=None):
+    import os
+    import sys
+    from datetime import datetime
+    from pathlib import Path
+    import logging
+    
+    import numpy as np
+    
+    # Use test data if no data provided
+    if not data:
+        angle_matrix = np.array([[-80, -80, -45, -45, 45, 45, 80, 80], 
+                                [-80, -80, -45, -45, 45, 45, 80, 80],
+                                [-45, -45, -10, -10, 10, 10, 45, 45],
+                                [-45, -45, -10, -10, 10, 10, 45, 45],
+                                [45, 45, 10, 10, -10, -10, -45, -45],
+                                [45, 45, 10, 10, -10, -10, -45, -45],
+                                [80, 80, 45, 45, -45, -45, 80, 80],
+                                [80, 80, 45, 45, -45, -45, 80, 80]])
+
+        data = {
+            'input': {
+                'angle_matrix': angle_matrix,
+            },
+            'task': {
+                'work_dir': Path('/home/nwen/simhub/client/test_work_dir').absolute(),
+                'data_dir': Path('/home/nwen/simhub/client/elmer_task/data').absolute(),
+            },
+            'metrics': {
+                'submit_timestamp': datetime.now(),
+            }
+        }
+     
+    workdir = data['task']['work_dir']
+
+    import gmsh
+    import pyvista as pv
+    
+    from scipy.spatial import cKDTree
+
+    from pyelmer import elmer
+    from pyelmer import execute
+    from pyelmer.post import scan_logfile
+    from objectgmsh.utils import add_physical_group, get_boundaries_in_box
+
+    wait_time = (datetime.now() - data['metrics']['submit_timestamp']).total_seconds()
+        
+    def cut_fragment(objectDimTags, toolDimTags):
+        import gmsh
+        factory = gmsh.model.occ
+
+        cutDimTags, _ = factory.cut(objectDimTags, toolDimTags, removeTool=False)
+        fragDimTags, _ = factory.fragment(cutDimTags, toolDimTags)
+        return cutDimTags, fragDimTags
+
+    gmsh.model.occ.cut_fragment = cut_fragment
+
+    ###############
+    os.environ['OMP_NUM_THREADS'] = '1'
+
+    start_time = datetime.now()
+
+    gmsh.initialize()
+    
+    gmsh.option.setNumber("General.Terminal", 1)
+    gmsh.model.add("heat-transfer-2d")
+    factory = gmsh.model.occ
+
+    mesh_size = 2.
+
+    def create_unit(rotation, offset, unit_size=(40., 40.), frame_width=1, stripe_width=2., stripe_offset=0., ignore_mass=1.):
+        unit_diag = np.sqrt(unit_size[0] ** 2 + unit_size[1] ** 2)
+        stripe_offset %= stripe_width
+        max_stripe_num = int(unit_diag / (stripe_width))
+
+        rotation = np.deg2rad(rotation)
+
+        stripe_dimtags = []
+        for i in range(-1, max_stripe_num + 1):
+            stripe_tag = factory.add_rectangle(0, i * stripe_width * 2 + stripe_offset, 0, unit_diag, stripe_width)
+            stripe_dimtags.append((2, stripe_tag))
+
+        factory.translate(stripe_dimtags, -unit_diag / 2, -unit_diag / 2, 0.)
+
+        factory.rotate(stripe_dimtags, 0., 0., 0., 0., 0., 1., rotation)
+
+        stripe_mask_tag = factory.add_rectangle(frame_width - unit_size[0] / 2, frame_width - unit_size[1] / 2, 0, 
+                                                unit_size[0] - frame_width * 2, unit_size[1] - frame_width * 2)
+
+        infill_dimtags, _ = factory.intersect([(2, stripe_mask_tag)], stripe_dimtags)
+
+        remove_dimtags = []
+        for dimtag in infill_dimtags:
+            if factory.get_mass(*dimtag) < ignore_mass:
+                remove_dimtags.append(dimtag)
+
+        for dimtag in remove_dimtags:
+            infill_dimtags.remove(dimtag)
+
+        factory.remove(remove_dimtags)
+
+        factory.synchronize()
+
+        factory.translate(infill_dimtags, unit_size[0] / 2 + offset[0], unit_size[1] / 2 + offset[1], 0.)
+
+        factory.synchronize()
+
+        return infill_dimtags
+
+    units = data['input']['angle_matrix']
+
+    unit_size = (40, 40)
+    board_margin = (10, 10)
+
+    units = np.flip(np.array(units).T, axis=1)
+    unit_size = np.array(unit_size)
+    board_margin = np.array(board_margin)
+    board_size = units.shape * unit_size + board_margin * 2
+
+    board_tag = factory.add_rectangle(0, 0, 0, *board_size)
+
+    stripe_dimtags = []
+
+    it = np.nditer(units, flags=['multi_index'])
+    for unit in it:
+        offset = it.multi_index * unit_size + board_margin
+        unit_dimtags = create_unit(unit, offset)
+        stripe_dimtags.extend(unit_dimtags)
+
+    factory.synchronize()
+
+    frame_dimtags, _ = factory.cut_fragment([(2, board_tag)], stripe_dimtags)
+
+    factory.synchronize()
+
+    frame_tags = [dimtag[1] for dimtag in frame_dimtags]
+    infill_tags = [dimtag[1] for dimtag in stripe_dimtags]
+
+    phy_frame = add_physical_group(2, frame_tags, "frame")
+    phy_infill = add_physical_group(2, infill_tags, "infill")
+
+    left_bound = get_boundaries_in_box(0., 0., 0., 0., board_size[1], 0., 2, frame_dimtags[0][1])
+    right_bound = get_boundaries_in_box(board_size[0], 0., 0., board_size[0], board_size[1], 0., 2, frame_dimtags[0][1])
+
+    phy_left = add_physical_group(1, [left_bound], "left_bound")
+    phy_right = add_physical_group(1, [right_bound], "right_bound")
+
+    # create mesh
+    gmsh.model.mesh.setSize(gmsh.model.getEntities(0), mesh_size)
+    gmsh.model.mesh.generate(2)
+    gmsh.write(str(workdir / 'case.msh'))
+    
+    mesh_retries = 2
+    
+    while (not (workdir / 'case.msh').exists()) or (workdir / 'case.msh').stat().st_size < 500:
+        if mesh_retries > 0:
+            print('Meshing failed, retrying...')
+            gmsh.model.mesh.generate(2)
+            gmsh.write(str(workdir / 'case.msh'))
+            mesh_retries -= 1
+        else:
+            raise ArithmeticError('Failed to mesh the geometries')
+            
+    print('Mesh generated')
+
+    mesh_time = datetime.now() - start_time
+    mesh_time = mesh_time.total_seconds()
+
+    start_time = datetime.now()
+
+    ###############
+    # elmer setup
+    elmer.data_dir = data['task']['data_dir']
+
+    sim = elmer.load_simulation("2D_steady")
+
+    pdms = elmer.load_material("PDMS", sim)
+    copper = elmer.load_material("copper", sim)
+
+    solver_heat = elmer.load_solver("HeatSolver", sim)
+    solver_output = elmer.load_solver("ResultOutputSolver", sim)
+    eqn = elmer.Equation(sim, "main", [solver_heat])
+
+    T0 = elmer.InitialCondition(sim, "T0", {"Temperature": 273.15})
+
+    body_frame = elmer.Body(sim, "frame", [phy_frame])
+    body_frame.material = copper
+    body_frame.initial_condition = T0
+    body_frame.equation = eqn
+
+    body_infill = elmer.Body(sim, "infill", [phy_infill])
+    body_infill.material = pdms
+    body_infill.initial_condition = T0
+    body_infill.equation = eqn
+
+    boundary_left = elmer.Boundary(sim, "left_bound", [phy_left])
+    boundary_left.data.update({"Temperature": 353.15})  # 80 °C
+    boundary_right = elmer.Boundary(sim, "right_bound", [phy_right])
+    boundary_right.data.update({"Temperature": 293.15})  # 20 °C
+
+    sim.write_startinfo(str(workdir))
+    sim.write_sif(str(workdir))
+
+    ##############
+    # execute ElmerGrid & ElmerSolver
+    execute.run_elmer_grid(str(workdir), "case.msh")
+    execute.run_elmer_solver(str(workdir))
+    
+    import subprocess
+    with open(data['task']['work_dir'] / 'debug.log', 'w') as fp:
+        sp = subprocess.run('ElmerSolver case.sif', shell=True, stdout=fp, stderr=fp)
+        fp.write(f'exit({sp.returncode})\n')
+        
+        sp = subprocess.run('module list', shell=True, stdout=fp, stderr=fp)
+        fp.write(f'exit({sp.returncode})\n')
+        
+        sp = subprocess.run('lscpu', shell=True, stdout=fp, stderr=fp)
+        fp.write(f'exit({sp.returncode})\n')
+        
+        sp = subprocess.run('env', shell=True, stdout=fp, stderr=fp)
+        fp.write(f'exit({sp.returncode})\n')
+    
+    print('FEM solved')
+      
+    
+    ###############
+    # scan log for errors and warnings
+    err, warn, stats = scan_logfile(str(workdir))
+    print("Errors:", err)
+    print("Warnings:", warn)
+    print("Statistics:", stats)
+
+    solve_time = datetime.now() - start_time
+    solve_time = solve_time.total_seconds()
+
+    start_time = datetime.now()
+
+    extraction_resolution = 4.5
+
+    m = pv.read(str(workdir / 'case_t0001.vtu'))
+    m.set_active_scalars('temperature')
+
+    kdtree = cKDTree(m.points.astype(np.double))
+
+    sampling_mesh = np.mgrid[0.:board_size[0]:extraction_resolution, 0.:board_size[1]:extraction_resolution]
+    sampling_spot = sampling_mesh.T.reshape((sampling_mesh.shape[1] * sampling_mesh.shape[2], 2))
+    dist, index = kdtree.query(np.hstack((sampling_spot, np.zeros((sampling_spot.shape[0], 1), dtype=sampling_spot.dtype))))
+
+    values = m.active_scalars[index]
+    result = np.hstack([sampling_spot, values.reshape((values.shape[0], 1))]).T
+    
+    print('Spatial data extracted')
+
+    extract_time = datetime.now() - start_time
+    extract_time = extract_time.total_seconds()
+    
+    data['output'] = {
+        'temperature_distribution': result,
+    }
+    data['output_files'] = {}
+    data['metrics'] |= {
+        'wait_time': wait_time,
+        'mesh_time': mesh_time,
+        'solve_time': solve_time,
+        'extract_time': extract_time,
+        'mesh_retries': 2 - mesh_retries,
+    }
+
+    return data
+
+
+if __name__ == '__main__':
+    from pprint import pprint
+    
+    result = run(None)
+    
+    pprint(result)
+    
